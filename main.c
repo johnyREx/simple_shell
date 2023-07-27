3d9e50dac0775b51a7ce4e2ca82e32667b2224f6
Celestine Wangechi <wangechicelestine@gmail.com>
2023-07-26 19:00:01 -0700

    few corrections

--- main.c
+++ main.c
@@ -16,9 +16,9 @@ int main(int ac, char **av, char *envp[])
 	char *path = NULL;
 	size_t buffer_size = 0;
 	ssize_t line_size = 0;
-	char **command, patha = NULL;
+	char **command, paths = NULL;
 	(void)envp;
-	void(av);
+	(void)av;
 
 	if (ac < 1)
 	{
@@ -50,8 +50,8 @@ int main(int ac, char **av, char *envp[])
 		}
 		if (check_builtin(command, line))
 		{
-			path = get_path_from__env();
-			paths - tokenize_string(path);
+			path = get_path_from__env;
+			paths = tokenize_string(path);
 			path_command = path_test_validity(paths, command[0]);
 		}
 		if (!path)
